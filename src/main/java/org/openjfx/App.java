bc8d302432ab6fc3e7a3a183f9298b21f9014f1c
tawhidulh1 <tawhidulh1@gmail.com>
2025-05-31 05:40:32 -0400

    5th commit

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -1,21 +1,20 @@
 package org.openjfx;
 
+import atlantafx.base.controls.Card;
+import atlantafx.base.controls.RingProgressIndicator;
+import atlantafx.base.theme.Dracula;
+import atlantafx.base.theme.Styles;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-
 
 /**
  * JavaFX App
@@ -24,60 +23,44 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-        BorderPane pane = new BorderPane();
-        
-        Image menu = new Image("https://static.thenounproject.com/png/345386-200.png");
-        
-          
-
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Button bt = new Button("Funds");
-        Button bt2 = new Button("Home");
-        Button bt3 = new Button("Connect");
-        HBox bar = new HBox(100);
-        bar.getChildren().addAll(bt,bt2,bt3);
-        bar.setAlignment(Pos.BOTTOM_CENTER);
-        
-
-        Rectangle rec = new Rectangle(600,200);
-        rec.setArcHeight(100);
-        rec.setArcWidth(100);
-        rec.setFill(Color.rgb(211,211,211));
-
-        Text welcText = new Text("Welcome!");
-        
-
-
-
-
-
-
-
-
-        
-        
-
-
-        
+        Application.setUserAgentStylesheet(new Dracula().getUserAgentStylesheet());
+        var pane = new BorderPane();
         var scene = new Scene(pane, 640, 480);
-        
+        var tabs = new TabPane(new Tab("Dashboard"), new Tab("Connect"), new Tab("Finance"));
 
-        pane.setBottom(bar);
-        pane.setCenter(rec);
-        pane.setTop(welcText);
-        pane.setAlignment(welcText, Pos.TOP_CENTER);
+        // TODO: fix center logic
+        var centerTop = new Group();
+        var centerBot = new Group();
+        var centerVbox = new VBox(
+                centerTop,
+                centerBot);
+
+        var bottomRightPane = new StackPane();
+        var indicator = new RingProgressIndicator(0.25, false);
+        var card = new Card();
+        bottomRightPane.setPrefHeight(800);
+        StackPane.setAlignment(indicator, Pos.TOP_RIGHT);
+        StackPane.setAlignment(card, Pos.BOTTOM_RIGHT);
+        // card.setBody(node);
+        card.setMinSize(300, 800);
+        card.setMaxSize(300, 800);
+
+        indicator.setMinSize(150, 150);
+        indicator.autosize();
+        centerTop.getChildren().add(indicator);
+        bottomRightPane.getChildren().add(card);
+
+        tabs.getStyleClass().add(Styles.TABS_CLASSIC);
+        tabs.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+
+        pane.setTop(tabs);
+        pane.setCenter(centerVbox);
+        pane.setBottom(bottomRightPane);
 
         stage.setScene(scene);
 
         stage.show();
- 
-        
-        
-        
+
     }
 
     public static void main(String[] args) {
